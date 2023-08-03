e5ac43210a8112141c823e2ba70e22fdf319b8e4
kamlesh <pkamleshkumar12@gmail.com>
2023-08-03 21:48:25 +0800

    Time: 5 ms (61.98%), Space: 43.9 MB (29.74%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,44 @@
+class Solution {
+    static Map<String, Integer> values = new HashMap<>();
+    static {
+        values.put("I", 1);
+        values.put("V", 5);
+        values.put("X", 10);
+        values.put("L", 50);
+        values.put("C", 100);
+        values.put("D", 500);
+        values.put("M", 1000);
+        values.put("IV", 4);
+        values.put("IX", 9);
+        values.put("XL", 40);
+        values.put("XC", 90);
+        values.put("CD", 400);
+        values.put("CM", 900);
+    }
+        
+    public int romanToInt(String s) {
+        int currentSum = 0;
+        int i = 0;
+        while(i <s.length()){
+            
+            if(i <s.length() - 1){
+                
+                String doubleSymbol = s.substring(i, i + 2);
+                
+                // check if this is the length 2 symbol case.
+                if(values.containsKey(doubleSymbol)){
+                    currentSum += values.get(doubleSymbol);
+                    i += 2;
+                    continue;
+                }
+            }
+            
+            // Otherwise, it must be the length-1 Symbol case.
+            String singleSymbol = s.substring(i, i + 1);
+            currentSum += values.get(singleSymbol);
+            i += 1; 
+        }
+        
+        return currentSum;
+    }
+}
